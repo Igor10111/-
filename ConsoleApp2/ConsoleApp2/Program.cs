862888eae18e0674a5b6f7478764a89ab0e71ae0
Igor10111 <igormalcev10@icloud.com>
2024-12-24 19:50:36 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        // Чтение числа от пользователя
+        Console.WriteLine("Введите трёхзначное число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Проверка, что число трёхзначное
+        if (number < 100 || number > 999)
+        {
+            Console.WriteLine("Ошибка: введите трёхзначное число!");
+            return;
+        }
+        // Извлечение цифр числа
+        int hundreds = number / 100;     // Первая цифра (сотни)
+        int tens = (number / 10) % 10;   // Вторая цифра (десятки)
+        int ones = number % 10;          // Третья цифра (единицы)
+
+        // Вычисление суммы и произведения цифр
+        int sum = hundreds + tens + ones;
+        int product = hundreds * tens * ones;
+
+        // Вывод результатов
+        Console.WriteLine($"Сумма цифр: " + sum);
+        Console.WriteLine($"Произведение цифр: " + product);
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину катета a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите длину катета b: ");
+        //Нахождение S
+        int b = Convert.ToInt32(Console.ReadLine());
+        double S = 0.5 * a * b;
+        Console.WriteLine(S);
+    }
+}
